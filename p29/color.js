c902ac2931fbd96178f3fb3cdc23466bf720c914
oyinkiwesley <wesleyoyinki6@gmail.com>
2024-08-27 10:41:46 +0100

    Add files via upload

---
+++ p29/color.js
@@ -0,0 +1,8 @@
+let colors = {
+    TextColor : '#f0f8ffb3',
+    border: 'grey',
+    subjectImageBackgroundColor : 'f0f8ffb3',
+    footer : '#333',
+    footerTextColor : '#fff',
+    footerbottom: 'footer-bottom'
+}
